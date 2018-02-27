1f471009eaa20f03374dbf1c706584cb66c55090
Gregoire Albizzati <gregalbizz@gmail.com>
2018-02-27 17:51:56 +0100

    huge refactoring and code restructuration
    add function to get the lafourchette Id of

--- robot/laFourchetteAPI.js
+++ robot/laFourchetteAPI.js
@@ -1,19 +1,39 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
 
-const fetchParameters = 
-{ 
-    method: 'GET',
-    headers: {},
-    follow: 20,
-    timeout: 0,
-    compress: true,
-    size: 0,
-    body: null,
-    agent: null
+//new String method to get rid of accents
+String.prototype.ereaseAccent = function(){
+    const accents = [
+        "\306","\346",                  //AE, ae
+        /[\300-\305]/g, /[\340-\345]/g, // A, a
+        /[\310-\313]/g, /[\350-\353]/g, // E, e
+        /[\314-\317]/g, /[\354-\357]/g, // I, i
+        /[\322-\330]/g, /[\362-\370]/g, // O, o
+        /[\331-\334]/g, /[\371-\374]/g, // U, u
+        /[\321]/g, /[\361]/g, // N, n
+        /[\307]/g, /[\347]/g, // C, c
+    ];
+    const noaccent = ["AE","ae",'A','a','E','e','I','i','O','o','U','u','N','n','C','c'];
+     
+    let str = this;
+
+    for(var i = 0; i < accents.length; i++){
+        str = str.replace(accents[i], noaccent[i]);
+    }
+    return str;
 }
 
-const getRestaurantsFrom = path =>
+//get the json from an api (the api has to return a json or it won't worked)
+const getJsonFromApi = async apiUrl =>
+{
+    const response = await fetch(apiUrl, "utf-8");
+    const content = response.json();
+
+    return content;
+}
+
+//get the restaurants from a Json file
+const getRestaurantsFromJson = path =>
 {
     const content = fs.readFileSync(path, 'utf-8');
     const arrayResataurants = JSON.parse(content);
@@ -21,34 +41,100 @@ const getRestaurantsFrom = path =>
     return arrayResataurants;
 }
 
-const getRestaurantIdFrom = async name =>
+//get the lafourchette id of a restaurant, return null if the restaurant doesn't exist
+const getIdFrom = async restaurant =>
 {
+    const name = restaurant.name.ereaseAccent();
+
     const apiUrl = "https://m.lafourchette.com/api/restaurant-prediction?name=" + name;
 
-    const response = await fetch(apiUrl, "utf-8", fetchParameters);
-    const content = response.json();
-    return content;
+    let isTimeout = true;
+
+    //while we have got a timeout we re-run the request
+    while(isTimeout)
+    {
+        try
+        {
+            const content = await getJsonFromApi(apiUrl);
+            const result = content.filter(r => restaurant.postalCode === r.address.postal_code);
+
+            isTimeout = false;
+            
+            if (result.length > 0)
+            {
+                return result[0].id;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        catch(e)
+        {
+            if(e.errno !== "ETIMEDOUT")
+            {
+                
+
+                isTimeout = false;
+                return null;
+            }
+            else
+            {
+                console.log("timeout at " + apiUrl);
+                console.log("this request will be re-send\n");
+            }
+        }
+    }
 }
 
-const getRestaurantOffersFrom = async id =>
+//get the offers of a restaurant with his id (return null if id is null)
+const getOffersFrom = async id =>
 {
-    const apiUrl = "https://m.lafourchette.com/api/restaurant/"+ id +"/sale-type";
+    if(id != null)
+    {
+        const apiUrl = "https://m.lafourchette.com/api/restaurant/"+ id +"/sale-type";
+        const offers = await getJsonFromApi(apiUrl);
 
-    const response = await fetch(apiUrl, "utf-8", fetchParameters);
-    const content = response.json();
-    return content;
+        const filteroffers = offers.filter(offer => offer.is_special_offer);
+    
+        return filteroffers;
+    }
+    else
+    {
+        return null;
+    }
 }
 
-const start = async () =>
+//function to create a restaurant whith the lafourchette features given a michelin restaurant
+const LaFourchetteRestaurant = async MichelinRestaurant =>
 {
-    //const arrayResataurants = getRestaurantsFrom("./data/restaurant.json");
+    const id = await getIdFrom(MichelinRestaurant);
+    const offers = await getOffersFrom(id);
 
-    const jardinRamparts = await getRestaurantIdFrom("Le Jardin des Remparts");
-    const id = jardinRamparts[0].id;
-
-    const offers = await getRestaurantOffersFrom(id);
-
-    console.log(offers);
+    return Object.assign(MichelinRestaurant,
+    {
+        id: id,
+        offers: offers
+    });
 }
 
-start();
+//get all the lafouchette data about restaurants from a json file, 
+//and return an array of restaurants with lafourchette features beside the michelin features already here.
+//if a restaurant is not in the lafourchette data base or if it doesn't have any offer : it won't be on the list
+const getLafourchetteData = async path =>
+{
+    console.log("reading data from " + path + " ...");
+    const michelinRestaurantArr = getRestaurantsFromJson(path);
+
+    console.log("get Lafourchette data...");
+    const promiseLafourchette = michelinRestaurantArr.map(r => LaFourchetteRestaurant(r));
+
+    console.log("process and wait all the api calls");
+    const lafourchetteRestaurantArr = await Promise.all(promiseLafourchette);
+
+    console.log("filter to keep the revelant restaurants")
+    const finalList = lafourchetteRestaurantArr.filter(r => r.offers !== null).filter(r => r.offers.length != 0);
+
+    console.log("---\tDONE\t---");
+    return finalList;
+}
