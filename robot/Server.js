eaaec6ff7ea87f7c406e04f6c4b3cbf6a329bbd2
Gregoire Albizzati <gregalbizz@gmail.com>
2018-03-02 16:11:49 +0100

    add react to render the view and server now run the api call before starting

--- robot/Server.js
+++ robot/Server.js
@@ -2,28 +2,40 @@ const express = require('express');
 const laFourchette = require('./laFourchetteAPI.js');
 
 const app = express();
+app.use(express.static('public'));
+
+//restaurants is null at the begining, therefor the server can
 let restaurants = null;
 
-async function launchServer()
+app.get('/', (request, response) =>
 {
-    restaurants = await laFourchette.getRestaurants("./data/restaurant.json");
+    response.sendFile("index.html");
+    console.log("send index.html to " + JSON.stringify(request));
+});
 
-    app.use(express.static('public'));
-
-    app.get('/', (request, response) =>
+app.get('/api/restaurants', (request, response) =>
+{
+    if(restaurants != null)
     {
-        response.sendFile("index.html");
-    });
-
-    app.get('/api/restaurants', (request, response) =>
+        response.json(restaurants);
+        console.log("send the restaurants to " + request);
+    }
+    else
     {
-        response.send(restaurants);
-    });
+        response.send("{error:\"the restaurants are not ready to be send !\"}");
+        console.log("restaurants are not ready for " + request);
+    }
+});
+
+//the server start listening when the restaurants have been processed
+laFourchette.getRestaurants("./data/restaurant.json").then(result => {
+
+    restaurants = result;
 
     app.listen(3000, function()
     {
+        console.log("restaurants have been process");
         console.log("listening on port 3000");
     });
-}
 
-launchServer();
+});
